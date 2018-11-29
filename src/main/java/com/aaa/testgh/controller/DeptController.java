36294c1f944ded98bb354be99dc6cf23b215462c
z1142726350 <1142726350@qq.com>
2018-11-29 09:32:55 +0800

    测试项目

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:lin
+ * createTime:2018-11-29 09:07
+ */
+public class DeptController {
+
+    public static void main(String[] args) {
+        System.out.println("测试gitgub...................");
+    }
+}
